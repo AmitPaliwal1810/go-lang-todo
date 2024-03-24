3901528ef06ee4955f8b22c55744e8740b8183f5
Amit Paliwal <amitpaliwal259@gmail.com>
2024-03-24 15:55:33 +0530

    initialized the server

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "mojo-jojo")
+	})
+
+	err := http.ListenAndServe(":5000", nil)
+
+	if err != nil {
+		fmt.Println("Error starting server", err)
+	}
 }
